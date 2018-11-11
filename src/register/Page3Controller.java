edca215d26c998b58a287053e35da5ec8023bcf9
Orayajp <jaipeng1003@gmail.com>
2018-11-11 22:48:06 +0700

    try to change color

--- src/register/Page3Controller.java
+++ src/register/Page3Controller.java
@@ -15,26 +15,19 @@ import java.util.ResourceBundle;
 public class Page3Controller implements Initializable {
     @FXML private TableView table;
     @FXML private TableColumn code,course,level;
-//    @FXML private TableColumn<FileInfo, String> subCode;
-//    @FXML private TableColumn<FileInfo, String> title;
-//    @FXML private TableColumn<FileInfo, String> level;
 
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-//       TableColumn subCode = new TableColumn("Subjects code");
-//       TableColumn title = new TableColumn("Course title");
-//       TableColumn level = new TableColumn("Level");
-//       table.getColumns().addAll(code,course,this.level);
-//       final ObservableList<FileInfo> fileInfos = FXCollections.observableArrayList(
-//                new FileInfo("20202020","Comsci","Red"));
-
        code.setCellValueFactory(new PropertyValueFactory<FileInfo,String>("code"));
        course.setCellValueFactory(new PropertyValueFactory<FileInfo,String>("name"));
        this.level.setCellValueFactory(new PropertyValueFactory<FileInfo,String>("color"));
-       table.setItems(getData());
-
-
+//       table.setItems(getData());
+//       TablePosition pos = (TablePosition) table.getSelectionModel().getSelectedCells().get(0);
+//       int index = pos.getRow();
+//       String selected = table.getItems().get(index).toString();
+//       System.out.println(selected);
+       setTable();
     }
     //cool it help
     public ObservableList<FileInfo> getData(){
@@ -47,5 +40,30 @@ public class Page3Controller implements Initializable {
         }
         readFile.closeFile();
         return list;
+    }
+
+
+    public void setTable(){
+        ObservableList <FileInfo> list = FXCollections.observableArrayList();
+        ReadFile readFile = new ReadFile("allSubject");
+        readFile.openFile();
+        readFile.readFile();
+        for(int i=0;i<readFile.getArraySize();i++){
+            list.add(new FileInfo(readFile.getSubjectCode(i),readFile.getSubjectName(i),readFile.getSubjectColor(i)));
+        }
+        readFile.closeFile();
+        table.setItems(list);
+//        for(int i =0;i<readFile.getArraySize();i++) {
+//            if(level.getCellObservableValue(i).getValue()=="3"){
+//                level.setStyle("-fx-background-color: red;");
+//            }
+//            else if (level.getCellObservableValue(i).getValue()=="2"){
+//                level.setStyle("-fx-background-color: green;");
+//            }
+//            else {
+//                level.setStyle("-fx-background-color: blue;");
+//            }
+//        }
+
     }
 }
